13fc3e1f598f3e42d30330670695f1122641a9b4
Lars Corneliussen <me@lcorneliussen.de>
2010-08-07 15:42:55 +0200

    Introduced common assembly info

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("lcorneliussen.de")]
+[assembly: AssemblyProduct("PatchPatch")]
+[assembly: AssemblyCopyright("Copyright © Lars Corneliussen 2010")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
