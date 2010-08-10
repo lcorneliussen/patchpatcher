10af26c4c71da5d44a704298153dd4ec1dc80ef3
Lars Corneliussen <me@lcorneliussen.de>
2010-08-10 09:37:29 +0200

    Added Assembly Configuration (Debug/Preview)

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -5,3 +5,9 @@
 [assembly: AssemblyCopyright("Copyright © Lars Corneliussen 2010")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
